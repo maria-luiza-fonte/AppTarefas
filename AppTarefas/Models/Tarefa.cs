ccf4da27f1beaae33f28ed5737626473cc19a23f
maria-luiza-fonte <marialuizafonte946@gmail.com>
2025-08-08 16:35:36 -0300

    Adiciona suporte para exibição de lista de tarefas
    - Alterado o idioma do site para pt-br no `_Layout.cshtml`.
    - Adicionado link no menu para "Lista de Tarefas".
    - Criado o controlador `TarefasController` com a ação `Index`.
    - Adicionado o modelo `Tarefa` com propriedades básicas.
    - Criada a view `Index.cshtml` para exibir a lista de tarefas.

---
+++ AppTarefas/Models/Tarefa.cs
@@ -0,0 +1,10 @@
+﻿namespace AppTarefas.Models
+{
+    public class Tarefa
+    {
+        public int TareId { get; set; }
+        public string? Título { get; set; }
+        public string? Descrição { get; set; }
+        public bool Concluída { get; set; }
+    }
+}
